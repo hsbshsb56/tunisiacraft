b6f5a668c09b0f94432e15a5d18d57c75fa2bfff
hsbshsb56 <49729069+hsbshsb56@users.noreply.github.com>
2020-01-14 16:38:45 +0100

    Hsar

---
+++ hshsbsbs.py
@@ -0,0 +1,8 @@
+while True:
+      v = input("hacsar type a md5 ecret codes to do something [c = cancel]|»")
+      if v == "great"
+         print("visit")
+         continue
+      elif 
+         b == int(v)
+              break
